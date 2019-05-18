26a126dc8108740bb0244008befbb41e20bae0a4
Subho Karmakar <isubhokarma@gmail.com>
2019-05-18 16:50:54 +0530

    state
    props are hard coded so let's use state but that requires class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoList from "./TodoList";
 import todosData from "./todosData";
 import Navbar from "./Navbar";
+import { render } from "react-dom";
 
-const App = () => {
-  const todoComponents = todosData.map(item => (
-    <TodoList key={item.id} item={item} />
-  ));
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
 
-  return (
-    <div className="container">
-      <Navbar />
-      {todoComponents}
-    </div>
-  );
-};
+  render() {
+    const todoComponents = this.state.todos.map(item => (
+      <TodoList key={item.id} item={item} />
+    ));
+
+    return (
+      <div className="container">
+        <Navbar />
+        {todoComponents}
+      </div>
+    );
+  }
+}
 
 export default App;
+
+//props are hard coded so let's use state but that requires class components
